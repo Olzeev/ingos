b3d937c3e7edcf458182fe653ee1d5a375b0dd16
Maxim <olzeevmax@gmail.com>
2024-03-10 21:40:03 +0300

    Authorization and fetching in one button

---
+++ packages/health/example/lib/main1.dart
@@ -0,0 +1,269 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:health/health.dart';
+import 'package:health_example/util.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(HealthApp());
+
+class HealthApp extends StatefulWidget {
+  @override
+  _HealthAppState createState() => _HealthAppState();
+}
+
+enum AppState {
+  DATA_NOT_FETCHED,
+  FETCHING_DATA,
+  DATA_READY,
+  NO_DATA,
+  AUTHORIZED,
+  AUTH_NOT_GRANTED,
+  DATA_ADDED,
+  DATA_DELETED,
+  DATA_NOT_ADDED,
+  DATA_NOT_DELETED,
+  STEPS_READY,
+}
+
+class _HealthAppState extends State<HealthApp> {
+  List<HealthDataPoint> _healthDataList = [];
+  AppState _state = AppState.DATA_NOT_FETCHED;
+  int _nofSteps = 0;
+
+  static final types = dataTypesAndroid;
+
+
+  final permissions = types.map((e) => HealthDataAccess.READ).toList();
+
+  HealthFactory health = HealthFactory(useHealthConnectIfAvailable: true);
+
+  Future authorize() async {
+
+    await Permission.activityRecognition.request();
+    await Permission.location.request();
+
+    // Check if we have health permissions
+    bool? hasPermissions =
+    await health.hasPermissions(types, permissions: permissions);
+
+    hasPermissions = false;
+
+    bool authorized = false;
+    if (!hasPermissions) {
+
+      try {
+        authorized =
+        await health.requestAuthorization(types, permissions: permissions);
+      } catch (error) {
+        print("Exception in authorize: $error");
+      }
+    }
+
+    setState(() => _state =
+    (authorized) ? AppState.AUTHORIZED : AppState.AUTH_NOT_GRANTED);
+  }
+
+  Future fetchData() async {
+    setState(() => _state = AppState.FETCHING_DATA);
+
+    final now = DateTime.now();
+    final yesterday = now.subtract(Duration(hours: 24));
+
+    // Clear old data points
+    _healthDataList.clear();
+
+    try {
+      // fetch health data
+      List<HealthDataPoint> healthData =
+      await health.getHealthDataFromTypes(yesterday, now, types);
+
+      _healthDataList.addAll(
+          (healthData.length < 100) ? healthData : healthData.sublist(0, 100));
+    } catch (error) {
+      print("Exception in getHealthDataFromTypes: $error");
+    }
+
+    // filter out duplicates
+    _healthDataList = HealthFactory.removeDuplicates(_healthDataList);
+
+    // print the results
+    _healthDataList.forEach((x) => print(x));
+
+    // update the UI to display the results
+    setState(() {
+      _state = _healthDataList.isEmpty ? AppState.NO_DATA : AppState.DATA_READY;
+    });
+  }
+
+  /// Fetch steps from the health plugin and show them in the app.
+  Future fetchStepData() async {
+    int? steps;
+
+    // get steps for today (i.e., since midnight)
+    final now = DateTime.now();
+    final midnight = DateTime(now.year, now.month, now.day);
+
+    bool stepsPermission =
+        await health.hasPermissions([HealthDataType.STEPS]) ?? false;
+    if (!stepsPermission) {
+      stepsPermission =
+      await health.requestAuthorization([HealthDataType.STEPS]);
+    }
+
+    if (stepsPermission) {
+      try {
+        steps = await health.getTotalStepsInInterval(midnight, now);
+      } catch (error) {
+        print("Caught exception in getTotalStepsInInterval: $error");
+      }
+
+      print('Total number of steps: $steps');
+
+      setState(() {
+        _nofSteps = (steps == null) ? 0 : steps;
+        _state = (steps == null) ? AppState.NO_DATA : AppState.STEPS_READY;
+      });
+    } else {
+      print("Authorization not granted - error in authorization");
+      setState(() => _state = AppState.DATA_NOT_FETCHED);
+    }
+  }
+
+  /// Revoke access to health data. Note, this only has an effect on Android.
+  Future revokeAccess() async {
+    try {
+      await health.revokePermissions();
+    } catch (error) {
+      print("Caught exception in revokeAccess: $error");
+    }
+  }
+
+  Widget _contentFetchingData() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+            padding: EdgeInsets.all(20),
+            child: CircularProgressIndicator(
+              strokeWidth: 10,
+            )),
+        Text('Fetching data...')
+      ],
+    );
+  }
+
+  Widget _contentDataReady() {
+    return ListView.builder(
+        itemCount: _healthDataList.length,
+        itemBuilder: (_, index) {
+          HealthDataPoint p = _healthDataList[index];
+          if (p.value is AudiogramHealthValue) {
+            return ListTile(
+              title: Text("${p.typeString}: ${p.value}"),
+              trailing: Text('${p.unitString}'),
+              subtitle: Text('${p.dateFrom} - ${p.dateTo}'),
+            );
+          }
+          if (p.value is WorkoutHealthValue) {
+            return ListTile(
+              title: Text(
+                  "${p.typeString}: ${(p.value as WorkoutHealthValue).totalEnergyBurned} ${(p.value as WorkoutHealthValue).totalEnergyBurnedUnit?.name}"),
+              trailing: Text(
+                  '${(p.value as WorkoutHealthValue).workoutActivityType.name}'),
+              subtitle: Text('${p.dateFrom} - ${p.dateTo}'),
+            );
+          }
+          if (p.value is NutritionHealthValue) {
+            return ListTile(
+              title: Text(
+                  "${p.typeString} ${(p.value as NutritionHealthValue).mealType}: ${(p.value as NutritionHealthValue).name}"),
+              trailing:
+              Text('${(p.value as NutritionHealthValue).calories} kcal'),
+              subtitle: Text('${p.dateFrom} - ${p.dateTo}'),
+            );
+          }
+          return ListTile(
+            title: Text("${p.typeString}: ${p.value}"),
+            trailing: Text('${p.unitString}'),
+            subtitle: Text('${p.dateFrom} - ${p.dateTo}'),
+          );
+        });
+  }
+
+  Widget _contentNoData() {
+    return Text('No Data to show');
+  }
+
+
+
+
+  Widget _stepsFetched() {
+    return Text('Total number of steps: $_nofSteps');
+  }
+
+
+
+  Widget _content() {
+    if (_state == AppState.DATA_READY)
+      return _contentDataReady();
+    else if (_state == AppState.FETCHING_DATA)
+      return _contentFetchingData();
+    else if (_state == AppState.STEPS_READY)
+      return _stepsFetched();
+    else return _contentNoData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Health Example'),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Wrap(
+                spacing: 10,
+                children: [
+                  TextButton(
+                      onPressed: authorize,
+                      child:
+                      Text("Auth", style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                          MaterialStatePropertyAll(Colors.blue))),
+                  TextButton(
+                      onPressed: fetchData,
+                      child: Text("Fetch Data",
+                          style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                          MaterialStatePropertyAll(Colors.blue))),
+
+                  TextButton(
+                      onPressed: fetchStepData,
+                      child: Text("Fetch Step Data",
+                          style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                          MaterialStatePropertyAll(Colors.blue))),
+                  TextButton(
+                      onPressed: revokeAccess,
+                      child: Text("Revoke Access",
+                          style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                          MaterialStatePropertyAll(Colors.blue))),
+                ],
+              ),
+              Divider(thickness: 3),
+              Expanded(child: Center(child: _content()))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
